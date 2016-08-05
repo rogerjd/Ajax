6594cfe1d7c820a5486a9529f95779f80569bd45
roger <rjpd@netzero.com>
2016-08-04 20:30:04 -0400

    init

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,44 @@
+﻿using Ajax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ajax.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string id)
+        {
+            //            return View("Index", id);
+            return View("Index", (object)id);
+        }
+
+        public ViewResult AppointmentData(string id)
+        {
+            IEnumerable<Appointment> data = new[]
+            {
+                new Appointment { ClientName="Joe", Date=DateTime.Parse("1/1/2012")},
+                new Appointment { ClientName="Joe", Date=DateTime.Parse("2/1/2012")},
+                new Appointment { ClientName="Joe", Date=DateTime.Parse("3/1/2012")},
+                new Appointment { ClientName="Jane", Date=DateTime.Parse("1/20/2012")},
+                new Appointment { ClientName="Jane", Date=DateTime.Parse("1/22/2012")},
+                new Appointment { ClientName="Bob", Date=DateTime.Parse("2/25/2012")},
+                new Appointment { ClientName="Bob", Date=DateTime.Parse("2/25/2013")}
+            };
+            if (!string.IsNullOrEmpty(id) && id != "All")
+            {
+                data = data.Where(a => a.ClientName == id);
+            }
+            return View(data);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ajax")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Ajax")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e077bb15-e64f-42a4-ad1f-2a72028fa11d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
